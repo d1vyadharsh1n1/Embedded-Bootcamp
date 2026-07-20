953a40ca8c3894a9898402b8b997d24649d4f0a6
Hari Prathyumnan K <99400770+Hari-K-03@users.noreply.github.com>
2026-07-20 11:16:31 +0530

    Complete implementation of 7 Segment Display
    Added a delay function and updated main loop to display digits 0-9 on a seven-segment display with delays.

--- Day_9/Seven_Segment/7segment.c
+++ Day_9/Seven_Segment/7segment.c
@@ -1,7 +1,37 @@
-#include <reg51.h>
+#include <reg52.h>
 
-void main()
-{
-    P0 = 0x3F;
-	
+void delay(unsigned int count);
+
+
+void main(void){
+	while(1){
+		P0=0x3F;		//0
+		delay(1000);
+		P0=0x06;		//1
+		delay(1000);
+		P0=0x5B;		//2
+		delay(1000);
+		P0=0x4F;		//3
+		delay(1000);
+		P0=0x66;		//4
+		delay(1000);
+		P0=0x6D;		//5
+		delay(1000);
+		P0=0x7D;		//6
+		delay(1000);
+		P0=0x07;		//7
+		delay(1000);
+		P0=0x7F;		//8
+		delay(1000);
+		P0=0x6F;		//9
+		delay(1000);
+		
+	}
+}
+
+void delay(unsigned int count){
+	unsigned int i,j;
+	for (i=0;i<count;i++){
+		for (j=0;j<112;j++);
+	}
 }
